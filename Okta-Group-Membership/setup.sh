c12feb2b3b67a7d9b9d9f51e802dd48dced84be1
Eric Z. Beard <ezbeard@amazon.com>
2022-10-25 13:53:40 -0700

    Minor comment change

--- Okta-Group-Membership/setup.sh
+++ Okta-Group-Membership/setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #
 # Set up any prerequisites needed for cfn test
+#
+# Currently we are setting up the user and group by hand.
+# If we create them here, we will have to also make changes
+# to the input files, since we can't use stack export values.
 
 echo "About to... TODO"
 
