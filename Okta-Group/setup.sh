9bc8897400bff2abba5002a3670706855073bcb5
Eric Beard <ezbeard@amazon.com>
2022-11-04 16:40:00 -0700

    Okta Group CICD

---
+++ Okta-Group/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# This script runs in the buildspec before testing
+
+# Replace the placeholder group name with something random
+cat example_inputs/inputs_1_create.json| sed "s/GROUP_NAME/$(uuidgen)/g" > inputs/inputs_1_create.json
+cat example_inputs/inputs_1_update.json| sed "s/GROUP_NAME/$(uuidgen)/g" > inputs/inputs_1_update.json
+
+
