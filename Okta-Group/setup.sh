ee360e8d52fe1c4511ca4d48a52b30d1574a2ebd
Eric Beard <ezbeard@amazon.com>
2022-11-07 11:01:21 -0800

    Use unique name in integ template

--- Okta-Group/setup.sh
+++ Okta-Group/setup.sh
@@ -5,5 +5,6 @@
 # Replace the placeholder group name with something random
 cat example_inputs/inputs_1_create.json| sed "s/GROUP_NAME/$(uuidgen)/g" > inputs/inputs_1_create.json
 cat example_inputs/inputs_1_update.json| sed "s/GROUP_NAME/$(uuidgen)/g" > inputs/inputs_1_update.json
+cat test/integ-template.yml| sed "s/GROUP_NAME/$(uuidgen)/g" > test/integ.yml
 
 
