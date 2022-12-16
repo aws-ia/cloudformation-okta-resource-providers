7b376d729c8724cf94d2485812962d074dfa60cf
Eric Beard <ezbeard@amazon.com>
2022-12-16 12:22:09 -0800

    Testing Group Association

--- Okta-Group-Application-Association/setup.sh
+++ Okta-Group-Application-Association/setup.sh
@@ -2,9 +2,8 @@
 #
 # This script runs in the buildspec before testing
 
-# Replace the placeholder group name with something random
-cat example_inputs/inputs_1_create.json| sed "s/GROUP_ID/$(uuidgen)/g" sed "s/APPLICATION_ID/$(uuidgen)/g" > inputs/inputs_1_create.json
-cat example_inputs/inputs_1_update.json| sed "s/GROUP_ID/$(uuidgen)/g" sed "s/APPLICATION_ID/$(uuidgen)/g" > inputs/inputs_1_update.json
-cat test/integ-template.yml| sed "s/GROUP_ID/$(uuidgen)/g" sed "s/APPLICATION_ID/$(uuidgen)/g" > test/integ.yml
+cat example_inputs/inputs_1_create.json| sed "s/GROUP_ID/${GROUP_ID}/g"| sed "s/APPLICATION_ID/${APPLICATION_ID}/g" > inputs/inputs_1_create.json
+cat test/integ.yml| sed "s/GROUP_ID/${GROUP_ID}/g"| sed "s/APPLICATION_ID/${APPLICATION_ID}/g" > test/integ-unique.yml
+python ../get_type_configuration.py
 
 
